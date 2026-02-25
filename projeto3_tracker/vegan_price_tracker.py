c76875aa5257468759ee71869c02041317733318
Deborah Ferreira <debahferreira2024@gmail.com>
2026-02-25 09:52:17 -0300

    :sparkle: feat: buscando os preços no site 1

--- projeto3_tracker/vegan_price_tracker.py
+++ projeto3_tracker/vegan_price_tracker.py
@@ -1,6 +1,7 @@
 #este projeto salva várias vezes os preços de um determinado produto em uma tabela e avisa quando há promoção
 
 import requests
+from bs4 import BeautifulSoup
 
 def main():
 
@@ -27,6 +28,15 @@ def main():
         except requests.exceptions.RequestException as e:
             print(f'Erro: {e}')
             i -= 1
+
+    #se tiver sucesso, continua com a lógica:
+    if(sucesso):
+        #pega dados brutos do site e transforma em uma árvore (parsing)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        #encontra o preço de um produto específico
+        for preco in soup.find_all('span'):
+            print(preco)
 
 
 if __name__ == "__main__":
